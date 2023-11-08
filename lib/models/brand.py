72800c9e9c682a2ebb7250a5f775f03dda6d6ef0
Lizz <lizzcklein@gmail.com>
2023-11-08 15:52:44 -0500

    small changes

--- lib/models/brand.py
+++ lib/models/brand.py
@@ -153,7 +153,6 @@ class Brand:
             if matches !=[]:
                 result.append(row)
         return result
-            # print(result)
 
     @classmethod
     def find_by_id(cls, id):
